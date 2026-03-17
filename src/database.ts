11b5bed9330ae8bcd90e3dac296f8edf08f710a2
Na2mduc123 <vutunam03@gmail.com>
2026-03-17 23:31:02 +0700

    fix login in railway

--- src/database.ts
+++ src/database.ts
@@ -2,5 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL ? "có" : "không có");
+
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 export const prisma = new PrismaClient({ adapter });
