8462bd1d9d256afadc4b6244aa88f591ddc256d7
Na2mduc123 <vutunam03@gmail.com>
2026-03-17 23:15:46 +0700

    fix build

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-    seed: "tsx prisma/seed.ts", // 👈 đổi ts-node → tsx
+    seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL!, // 👈 thêm vào
+    url: process.env.DATABASE_URL!,
   },
 });
--- src/database.ts
+++ src/database.ts
@@ -3,5 +3,4 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
-
 export const prisma = new PrismaClient({ adapter });
